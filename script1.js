00d50a68f99ef1a70291d4e991ec3e985fdd6ce8
coudert <jean.baptiste.coudert1@gmail.com>
2019-09-09 14:28:03 +0200

    init2

---
+++ script1.js
@@ -0,0 +1,11 @@
+// var name1Element = "name1";
+// alert(name1Element);
+// console.log(name1Element);
+
+// var lastname = "Doe";
+// var name = "John";
+// var city ="New-York";
+// alert("Nom : " + lastname + "\n" + "Prenom : " + name + "\n" + "Ville : " + city);
+
+var nameInput = prompt("Entre ton prénom : ")
+alert("Bonjour " + nameInput)
