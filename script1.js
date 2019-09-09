6840bc6f92520ada9f11c2607c62561edb7a38b3
léni <lenipep@hotmail.fr>
2019-09-09 15:48:43 +0200

    innit 3

--- script1.js
+++ script1.js
@@ -1,11 +1,40 @@
+// exercice 1
 // var name1Element = "name1";
 // alert(name1Element);
 // console.log(name1Element);
 
+//exercice 2
 // var lastname = "Doe";
 // var name = "John";
 // var city ="New-York";
 // alert("Nom : " + lastname + "\n" + "Prenom : " + name + "\n" + "Ville : " + city);
 
-var nameInput = prompt("Entre ton prénom : ")
-alert("Bonjour " + nameInput)
+//exercice 3
+//var nameInput = prompt("Entre ton prénom : ")
+//alert("Bonjour " + nameInput)
+
+
+//exercice 4
+
+// var nomInput = document.getElementById("nom")
+// var prenomInput = document.getElementById("prenom")
+// var villeInput = document.getElementById("ville") 
+
+// var btnElement = document.getElementById("valider")
+// btnElement.addEventListener("click", function(){
+//     alert("nom : " + nomInput.value  + "\n" + "prenom : " + prenomInput.value  + "\n" + "ville : " + villeInput.value );
+// });
+
+
+//exerice 5
+// console.log("mfaemf")
+// var firstNumber = document.getElementById("premier_nombre")
+// var secondNumber = document.getElementById("deuxieme_nombre")
+
+// var btnElement = document.getElementById("valider")
+// btnElement.addEventListener("click", function(){
+//     //alert(parseInt(firstNumber.value) * secondNumber.value)
+//           //or
+//             var result = parseInt(firstNumber.value) * secondNumber.value;
+//             alert(result)
+// });
